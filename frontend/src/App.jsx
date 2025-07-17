4590d48dcd092580dbfde8ab19810893165444a5
nikita2999 <nikkitabhardwaj@gmail.com>
2025-07-17 15:44:53 +0530

    new menu page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
+import Menu from './pages/Menu/Menu'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
@@ -16,6 +17,7 @@ const App = () => {
       <Routes>
     < Route path='/' element={<Home/>} />
     <Route path='/cart' element={<Cart/>}/>
+      <Route path='/menu' element={<Menu/>}/>
     <Route path='/order' element={<PlaceOrder/>}/>
       </Routes>
     </div>
